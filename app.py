f986cff0aa1014b1573b9f4b6dfec16ef3c67f8e
ShreyaKushal <89073166+ShreyaKushal@users.noreply.github.com>
2022-10-02 17:16:27 +0800

    Update app.py (read roles and skills)
    Co-Authored-By: triyulianni <89073709+triyulianni@users.noreply.github.com>

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root' + \
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root' + \
                                         '@localhost:3306/spm'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100,
@@ -18,14 +18,37 @@ class JobRoles(db.Model):
     JobRole_ID = db.Column(db.String(50), primary_key=True)
     JobRole_Name = db.Column(db.String(50))
 
-db.create_all()
+
+    def __init__(self, JobRole_ID, JobRole_Name): #initialise value of job role record, specify properties of a job role when it is created
+        self.JobRole_ID = JobRole_ID
+        self.JobRole_Name = JobRole_Name
+  
+ 
+    def json(self): #returns json representation of the job role
+        return {"JobRole_ID": self.JobRole_ID, "JobRole_Name": self.JobRole_Name}
+#db.create_all()
+
+class Skills(db.Model):
+    __tablename__ = 'Skills'
+    Skill_ID = db.Column(db.String(50), primary_key=True)
+    Skill_Name = db.Column(db.String(50), nullable=False)
+    Skill_Desc = db.Column(db.String(255), nullable=False)
+
+    def __init__(self, Skill_ID, Skill_Name, Skill_Desc): #initialise value of job role record, specify properties of a job role when it is created
+        self.Skill_ID = Skill_ID
+        self.Skill_Name = Skill_Name
+        self.Skill_Desc = Skill_Desc
+ 
+    def json(self): #returns json representation of the job role
+        return {"Skill_ID": self.Skill_ID, "Skill_Name": self.Skill_Name, "Skill_Desc": self.Skill_Desc}
+
 
 @app.route("/JobRoles")
-def JobRole_by_id(JobRole_ID):
-    JobRole = JobRoles.query.filter_by(JobRole_ID=JobRole_ID).first()
-    if JobRole:
+def get_all_JobRoles():
+    JobRole = JobRoles.query.all()
+    if len(JobRole):
         return jsonify({
-            "data": JobRoles.to_dict()
+            "data": [JobRoles.json() for JobRoles in JobRole]
         }), 200
     else:
         return jsonify({
@@ -33,15 +56,28 @@ def JobRole_by_id(JobRole_ID):
         }), 404
 
 
+@app.route("/Skills")
+def get_all_Skills():
+    Skill = Skills.query.all()
+    if len(Skill):
+        return jsonify({
+            "data": [Skills.json() for Skills in Skill]
+        }), 200
+    else:
+        return jsonify({
+            "message": "Skill not found."
+        }), 404
 
-
-
-
-
-
-
-
-
+# def JobRole_by_id(JobRole_ID):
+#     JobRole = JobRoles.query.filter_by(JobRole_ID=JobRole_ID).all()
+#     if JobRole:
+#         return jsonify({
+#             "data": [JobRoles.json() for JobRoles in JobRole]
+#         }), 200
+#     else:
+#         return jsonify({
+#             "message": "Job Role not found."
+#         }), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
