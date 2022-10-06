1e263988eaa74e884246bb84ad014249681c0dd2
ShreyaKushal <89073166+ShreyaKushal@users.noreply.github.com>
2022-10-07 01:44:09 +0800

    trying things to assign courses

--- app.py
+++ app.py
@@ -30,17 +30,39 @@ class JobRoles(db.Model):
 
 class Skills(db.Model):
     __tablename__ = 'Skills'
-    Skill_ID = db.Column(db.String(50), primary_key=True)
-    Skill_Name = db.Column(db.String(50), nullable=False)
-    Skill_Desc = db.Column(db.String(255), nullable=False)
+    Course_ID = db.Column(db.String(50), primary_key=True)
+    Course_Name = db.Column(db.String(50), nullable=False)
+    Course_Desc = db.Column(db.String(255), nullable=False)
 
-    def __init__(self, Skill_ID, Skill_Name, Skill_Desc): #initialise value of job role record, specify properties of a job role when it is created
-        self.Skill_ID = Skill_ID
-        self.Skill_Name = Skill_Name
-        self.Skill_Desc = Skill_Desc
+    def __init__(self, Course_ID, Course_Name, Course_Desc): #initialise value of job role record, specify properties of a job role when it is created
+        self.Course_ID = Course_ID
+        self.Course_Name = Course_Name
+        self.Course_Desc = Course_Desc
  
     def json(self): #returns json representation of the job role
-        return {"Skill_ID": self.Skill_ID, "Skill_Name": self.Skill_Name, "Skill_Desc": self.Skill_Desc}
+        return {"Course_ID": self.Course_ID, "Course_Name": self.Course_Name, "Course_Desc": self.Course_Desc}
+        
+class Courses(db.Model):
+    __tablename__ = 'Courses'
+    Course_ID = db.Column(db.String(50), primary_key=True)
+    Course_Name = db.Column(db.String(50), nullable=False)
+    Course_Desc = db.Column(db.String(255), nullable=False)
+    Course_Status = db.Column(db.String(15), nullable=False)
+    Course_Type = db.Column(db.String(10), nullable=False)
+    Course_Category = db.Column(db.String(50), nullable=False)
+
+    def __init__(self, Course_ID, Course_Name, Course_Desc, Course_Status, Course_Type, Course_Category): #initialise value of job role record, specify properties of a job role when it is created
+        self.Course_ID = Course_ID
+        self.Course_Name = Course_Name
+        self.Course_Desc = Course_Desc
+        self.Course_Status = Course_Status
+        self.Course_Type = Course_Type
+        self.Course_Category = Course_Category
+ 
+    def json(self): #returns json representation of the job role
+        return {"Course_ID": self.Course_ID, "Course_Name": self.Course_Name, "Course_Desc": self.Course_Desc, 
+        "Course_Status":self.Course_Status, "Course_Type":self.Course_Type, "Course_Category": self.Course_Category}
+
 
 
 @app.route("/JobRoles")
@@ -66,6 +88,18 @@ def get_all_Skills():
     else:
         return jsonify({
             "message": "Skill not found."
+        }), 404
+
+@app.route("/Courses")
+def get_all_Courses():
+    Course = Courses.query.all()
+    if len(Course):
+        return jsonify({
+            "data": [Courses.json() for Courses in Course]
+        }), 200
+    else:
+        return jsonify({
+            "message": "Course not found."
         }), 404
 
 # def JobRole_by_id(JobRole_ID):
