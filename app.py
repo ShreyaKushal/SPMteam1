15c2aadd2ac29635a49a242e294636d4493d6006
JasmineLee-ops <jasminelee.2020@scis.smu.edu.sg>
2022-10-19 21:50:47 +0800

    Revert "removed root from app.py"
    This reverts commit 3c10acfe273656155b5518204ff6127042db5796.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root' + \
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root' + \
                                         '@localhost:3306/spm'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100,
